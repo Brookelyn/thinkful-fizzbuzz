a0c9c5b03abe985c65a2c4eecd1a7246a3ea837a
Brookelyn <brooke.balza@gmail.com>
2015-03-07 13:55:09 +0000

    Fizzbuzz refactoring
    Refactoring Fizzbuzz solution in order to prompt user to input number to
    count to and ensure user has supplied an int, not a fraction or string.

--- fizzbuzz-solution.js
+++ fizzbuzz-solution.js
@@ -1,14 +1,33 @@
-for (var i = 0; i <= 100; i++) {
-	if ((i % 3 === 0) && (i % 5 === 0)) {
-		console.log('fizzbuzz');
+$(document).ready(function() {
+	var number = prompt('Please enter a number');
+	var number = +"number";
+	if((number != 'number') || (number % 1 != 0)) {
+		alert("Oops, that's not a number! Please try again.");
+		var number = prompt('Please enter a number');
 	}
-	else if ((i % 3 === 0) && (i % 5 != 0)) {
-		console.log('fizz');
-	}
-	else if ((i % 5 === 0) && (i % 3 != 0)) {
-		console.log('buzz');
-	}
+
 	else {
-		console.log(i);
+
+	function fizzbuzz(number) {
+		for (var i = 0; i <= number; i++) {
+			if ((i % 3 === 0) && (i % 5 === 0)) {
+				console.log('fizzbuzz');
+			}
+			else if ((i % 3 === 0) && (i % 5 != 0)) {
+				console.log('fizz');
+			}
+			else if ((i % 5 === 0) && (i % 3 != 0)) {
+				console.log('buzz');
+			}
+			else {
+				console.log(i);
+			}
+		}		
 	}
-}
+
+	}
+
+	fizzbuzz(number);
+
+});
+
