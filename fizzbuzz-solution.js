4a3516cc6d15c613efd9cc81f0d0357b126bf530
Brookelyn <brooke.balza@gmail.com>
2015-03-09 15:42:08 +0000

    Incorporating Justin's changes

--- fizzbuzz-solution.js
+++ fizzbuzz-solution.js
@@ -1,33 +1,54 @@
 $(document).ready(function() {
-	var number = prompt('Please enter a number');
-	var number = +"number";
-	if((number != 'number') || (number % 1 != 0)) {
-		alert("Oops, that's not a number! Please try again.");
-		var number = prompt('Please enter a number');
+	var number;
+	
+	function getUserInput() {
+		number = prompt('Please enter a number');
+		number = +"number";
+		if(canContinue()) {
+			fizzbuzz(number);
+		};
+
+		else {
+			alert("Oops, that's not a number! Please try again.");
+			getUserInput();
+		}
 	}
 
-	else {
+	function canContinue() {
+		var bool = true;
+		if (typeof number != 'number') {
+			bool = false;
+		}
+		else if (number % 1 != 0) {
+			bool = false;
+		} 
+		else if (isNaN(number)) {
+			bool = false;
+		}
+		return bool;
+	}
 
 	function fizzbuzz(number) {
+		var output = '';
 		for (var i = 0; i <= number; i++) {
 			if ((i % 3 === 0) && (i % 5 === 0)) {
-				console.log('fizzbuzz');
+				output = 'fizzbuzz';
 			}
-			else if ((i % 3 === 0) && (i % 5 != 0)) {
-				console.log('fizz');
+			else if (i % 3 === 0) {
+				output = 'fizz';
 			}
-			else if ((i % 5 === 0) && (i % 3 != 0)) {
-				console.log('buzz');
+			else if (i % 5 === 0) {
+				output = 'buzz';
 			}
 			else {
-				console.log(i);
+				output = i;
 			}
+			console.log(output);
+			$('body').append('<p>' + output + '</p>');
 		}		
 	}
-
-	}
-
-	fizzbuzz(number);
+	
+	getUserInput();
 
 });
 
